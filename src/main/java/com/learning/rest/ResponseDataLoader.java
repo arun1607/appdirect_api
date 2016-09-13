6da2b7b85e6fe1f31e45ba5040d886cf086ebbe2
Shrivastava Amit <amit.shrivastava@fidelis-inc.com>
2016-09-13 18:15:19 +0530

    Replaced rest remplate with httpClient

--- src/main/java/com/learning/rest/ResponseDataLoader.java
+++ src/main/java/com/learning/rest/ResponseDataLoader.java
@@ -1,9 +1,16 @@
 package com.learning.rest;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 /**
  * Created by amits on 13/09/16.
@@ -12,13 +19,31 @@ import org.springframework.web.client.RestTemplate;
 @Log4j2
 public class ResponseDataLoader {
 
-    @Autowired
-    private RestTemplate restTemplate;
 
+    public void fetchResponse(String url) throws IOException {
 
-    public void fetchResponse(String url) {
-        String response = restTemplate.getForObject(url, String.class);
-        log.info("Response is : {}", response);
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        try {
+            HttpGet httpget = new HttpGet(url);
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            log.info("Response is : {}", responseBody);
+        } finally {
+            httpclient.close();
+        }
     }
 
 }
